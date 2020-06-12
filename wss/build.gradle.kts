c0f24e275f4eec381b874d99bf8cd8171e6a7df6
PrefixTim <developer.of.gift@gmail.com>
2020-06-11 23:34:34 -0700

    wss project created Package created

---
+++ wss/build.gradle.kts
@@ -0,0 +1,15 @@
+group = "club.csmrobotics"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("mysql:mysql-connector-java:8.0.19")
+    implementation("org.apache.commons:commons-dbcp2:2.7.0")
+    implementation("org.springframework.data:spring-data-solr")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
---
+++ wss/src/main/java/club/csmrobotics/csmrcserver/wss/domain/data/Package.java
@@ -0,0 +1,37 @@
+package club.csmrobotics.csmrcserver.wss.domain.data;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * <p>
+ * simple class representing data from the ras pi
+ * must be separated to different files in later versions
+ * variables names are unpleasant
+ * </p>
+ */
+@lombok.Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Package {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date date;
+    private float temp;
+    private float pressPa;
+    private float hum;
+    private float CO2ppm;
+    private float tVOCpp;
+    private long count;
+    private long packetNum;
+    private int deviceInfo;
+    @Version
+    private Long version;
+}
